75922d6f993c194c359411ea6d44138b0fd6eb5b
alicia-myob <Alicia.Shen@myob.com>
2020-11-25 13:40:30 +1300

    [010][feat] Setting up Hand constructor

--- BlackJack/Hand.cs
+++ BlackJack/Hand.cs
@@ -1,7 +1,22 @@
+using System;
+
 namespace BlackJack_Kata
 {
     public class Hand
     {
-        
+        private Deck _currentDeck;
+        private bool[] _taken;
+        private Random _cardGen;
+        public Hand(Deck deck)
+        {
+            _currentDeck = deck;
+            _taken = new bool[52];
+            _cardGen = new Random();
+        }
+
+        public void InitialHand()
+        {
+            
+        }
     }
 }
