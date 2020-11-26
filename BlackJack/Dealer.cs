1715c9199d263a7bf91f78b69b2eb3604d144e3f
alicia-myob <Alicia.Shen@myob.com>
2020-11-26 14:16:16 +1300

    [022][feat] Giving Dealer create and shuffle deck actions

--- BlackJack/Dealer.cs
+++ BlackJack/Dealer.cs
@@ -1,7 +1,59 @@
+using System;
+using System.Collections.Generic;
+using Xunit.Sdk;
+
 namespace BlackJack_Kata
 {
     public class Dealer
     {
+        private Card[] _deck;
+        public Dealer()
+        {
+            
+        }
+
+        public void CreateDeck()
+        {
+            _deck = new Card[52];
+        }
         
+        public void ShuffleDeck()
+        {
+            var shuffledDeck = new Card[52];
+            var place = new Random();
+            var cardExists = new bool[52];
+
+            for (var i = 0; i < 52; i++)
+            {
+                var cardPlace = 0;
+                var foundPlace = false;
+                while (!foundPlace)
+                {
+                    cardPlace = place.Next(52);
+                    if (!cardExists[cardPlace])
+                    {
+                        foundPlace = true;
+                    }
+                }
+
+                cardExists[cardPlace] = true;
+                try
+                {
+                    shuffledDeck[cardPlace] = _deck[i];
+                } catch (NullException){}
+                
+            }
+            
+            shuffledDeck.CopyTo(_deck, 0);
+        }
+
+        public void PrepareDeck(Card[] deck)
+        {
+            var gameDeck = new Queue<Card>();
+            foreach (var card in deck)
+            {
+                gameDeck.Enqueue(card);
+            }
+        }
     }
 }
--- BlackJack/Program.cs
+++ BlackJack/Program.cs
@@ -10,22 +10,15 @@ namespace BlackJack_Kata
             
             Console.WriteLine("Welcome to BlackJack!");
             Program blackjackGame = new Program();
-            var deck = new Deck();
-            deck.Shuffle();
-            blackjackGame.StartGame(deck);
+            
+            blackjackGame.StartGame();
         }
 
-        public void StartGame(Deck deck)
-        {
-            var gameDeck = new Queue<Card>();
-            var currentDeck = deck.GetDeck();
-            foreach (var card in currentDeck)
-            {
-                gameDeck.Enqueue(card);
-            }
-            var newTable = new Table(gameDeck);
+        public void StartGame()
+        { 
+            var dealer = new Dealer();
+            var player = new Player();
         }
-        
     }
     
 }
