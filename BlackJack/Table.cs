b9895b29c89131d59a556ea67be44a1928be05ff
alicia-myob <Alicia.Shen@myob.com>
2020-11-26 16:38:02 +1300

    [031][feat] Adding score and card displaying properties to Table.cs

--- BlackJack/Table.cs
+++ BlackJack/Table.cs
@@ -4,12 +4,17 @@ namespace BlackJack_Kata
 {
     public class Table
     {
-        
+        private List<Card> _playerCards;
+        private List<Card> _dealerCards;
+        private int _playerScore = 0;
+        private int _dealerScore = 0;
         public Table()
         {
-           
-            
+           _playerCards = new List<Card>();
+           _dealerCards = new List<Card>();
         }
+        
+        
 
         
         
