178c914819bd7fe401f6cd1f1b0d91fe82b30a76
alicia-myob <Alicia.Shen@myob.com>
2020-11-26 13:44:20 +1300

    [019][fix] Taking Dealer actions out of Table class

--- BlackJack/Table.cs
+++ BlackJack/Table.cs
@@ -4,30 +4,14 @@ namespace BlackJack_Kata
 {
     public class Table
     {
-        private Queue<Card> _currentDeck;
-        private Hand _playerHand;
-        private Hand _dealerHand;
-        public Table(Queue<Card> currentDeck)
+        
+        public Table()
         {
-            _currentDeck = currentDeck;
-            _playerHand = new Hand();
-            _dealerHand = new Hand();
-            DealInitialCards();
+           
+            
         }
 
-        public void DealInitialCards()
-        {
-            //Deal player cards
-            _playerHand.StoreCardInHand(_currentDeck.Dequeue());
-            _playerHand.StoreCardInHand(_currentDeck.Dequeue());
-            
-            //Deal dealer cards
-            _dealerHand.StoreCardInHand(_currentDeck.Dequeue());
-            _dealerHand.StoreCardInHand(_currentDeck.Dequeue());
-            
-            _playerHand.ShowHand();
-            _dealerHand.ShowHand();
-        }
+        
         
         
         
