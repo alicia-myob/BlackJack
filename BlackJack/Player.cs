5e2a10c1f41e85edbaeae35ff9f316c4a40486a5
alicia-myob <Alicia.Shen@myob.com>
2020-11-26 15:40:10 +1300

    [024][feat] Setting some Player properties

--- BlackJack/Player.cs
+++ BlackJack/Player.cs
@@ -2,6 +2,11 @@ namespace BlackJack_Kata
 {
     public class Player
     {
-        
+        private Hand _playerHand = new Hand();
+        private int _handWorth = 0;
+        public Player()
+        {
+            
+        }
     }
 }
