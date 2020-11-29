4e9f3d38def848b79134a91e83ad8533c6559ff5
alicia-myob <Alicia.Shen@myob.com>
2020-11-30 11:03:52 +1300

    [040][feat] Adding a player to the table and announcing their current stats

--- BlackJack/Table.cs
+++ BlackJack/Table.cs
@@ -1,18 +1,53 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace BlackJack_Kata
 {
     public class Table
     {
+        private List<Player> _playersAtTable = new List<Player>();
         private List<Card> _playerCards;
         private List<Card> _dealerCards;
-        private int _playerScore = 0;
-        private int _dealerScore = 0;
+        
         public Table()
         {
            _playerCards = new List<Card>();
            _dealerCards = new List<Card>();
         }
+
+        public void AddPlayerToTable(Player player)
+        {
+            _playersAtTable.Add(player);
+        }
+
+        public void announceScore(Player player)
+        {
+            Console.WriteLine("\nYou are at currently at " + player.PassScore());
+            Console.Write("with the hand [");
+            foreach (var card in player.GetHand())
+            {
+                if (card.Equals(player.GetHand().Last()))
+                {
+                    Console.Write(card.ToString() + "]\n");
+                }
+                else
+                {
+                    Console.Write(card.ToString() + ", ");
+                }
+            }
+            
+        }
+
+        public void UpdateTable()
+        {
+            foreach (var player in _playersAtTable)
+            {
+                announceScore(player);
+            }
+        }
+
+        
         
         
 
