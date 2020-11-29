5ab05d4985ba20fdf49c661ec599db2e58d612d5
alicia-myob <Alicia.Shen@myob.com>
2020-11-30 11:47:36 +1300

    [044][feat] Checking user input is 0 or 1

--- BlackJack/InputValidator.cs
+++ BlackJack/InputValidator.cs
@@ -1,7 +1,15 @@
+using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
 namespace BlackJack_Kata
 {
     public static class InputValidator
     {
-        
+        public static bool CheckHitStayInput(string input)
+        {
+            var rx = new Regex("^\\s*[01]\\s*$");
+            return (rx.IsMatch(input));
+        }
     }
 }
