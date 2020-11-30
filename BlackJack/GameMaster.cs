a672362505a5d7ec76fe4015db5508cca6e2a936
alicia-myob <Alicia.Shen@myob.com>
2020-11-30 16:22:05 +1300

    [048][feat] Creating a GameMaster.cs to control the logic of the game

--- BlackJack/GameMaster.cs
+++ BlackJack/GameMaster.cs
@@ -2,6 +2,35 @@ namespace BlackJack_Kata
 {
     public class GameMaster
     {
+        private Card[] _deck;
+        public GameMaster()
+        {
+            PrepareGame();
+        }
+
+        private void PrepareGame()
+        {
+            CreateDeck();
+            var dealer = new Dealer(_deck, new BasicShuffler(new BasicRandomGenerator()));
+        }
+        
+        private void CreateDeck()
+        {
+            _deck = new Card[52];
+            for (var suit = 0; suit < 4; suit++)
+            {
+                for (var rank = 1; rank < 14; rank++)
+                {
+                    _deck[suit*13 + rank - 1] = new Card((Suit) suit, (Rank)rank);
+                }
+            }
+        }
+
+        private void AssignDealerToTable(Dealer dealer)
+        {
+            
+        }
+        
         
     }
 }
