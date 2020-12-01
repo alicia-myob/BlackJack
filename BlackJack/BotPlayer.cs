9be5486fad9b8916a3c141db0f3f7256622e5996
alicia-myob <Alicia.Shen@myob.com>
2020-12-01 15:36:44 +1300

    [070][feat] Adding a decision maker to the bot

--- BlackJack/BotPlayer.cs
+++ BlackJack/BotPlayer.cs
@@ -2,6 +2,19 @@ namespace BlackJack_Kata
 {
     public class BotPlayer : Player
     {
+        public BotPlayer(){}
+
+        public bool DecisionMaker()
+        {
+            if (_score < 17)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
         
     }
 }
