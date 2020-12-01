1a6f4dda9b3e6dcb4d843fbb52fbc989f7f6779d
alicia-myob <Alicia.Shen@myob.com>
2020-12-01 14:12:47 +1300

    [061][feat] Adding game play feature to GameMaster

--- BlackJack/GameMaster.cs
+++ BlackJack/GameMaster.cs
@@ -3,6 +3,7 @@ namespace BlackJack_Kata
     public class GameMaster
     {
         private Card[] _deck;
+        private const int InitialNoOfCards = 2;
         public GameMaster()
         {
             PrepareGame();
@@ -14,6 +15,8 @@ namespace BlackJack_Kata
             var dealer = new Dealer(_deck, new BasicShuffler(new BasicRandomGenerator()));
             AssignDealerToTable(dealer);
             var player = new Player();
+            dealer.DealCardToPlayer(player, InitialNoOfCards);
+            GamePlay(dealer,player);
         }
         
         private void CreateDeck()
@@ -33,7 +36,46 @@ namespace BlackJack_Kata
             var table = new Table();
             dealer.SetTable(table);
         }
-        
+
+        private static void GamePlay(Dealer dealer, Player player)
+        {
+            var table = dealer.GetTable();
+            while (player.PlayerScoreUnder21())
+            {
+                var playerScore = ValueCalculator.HandWorth(player.GetHand());
+                player.ReceiveScore(playerScore);
+                table.AnnounceScore(player);
+                
+                if (player.PlayerScoreIs21())
+                {
+                    break;
+                }
+
+                var hitOrStay = dealer.AskHitOrStay();
+                if (hitOrStay == 1)
+                {
+                    dealer.DealCardToPlayer(player, 1);
+                    table.AnnounceDrawnCard(player);
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            if (player.PlayerScoreIs21())
+            {
+                table.Congratulations();
+            } else if (!player.PlayerScoreUnder21())
+            {
+                table.Busted();
+            }
+            else
+            {
+                
+            }
+
+        }
         
     }
 }
