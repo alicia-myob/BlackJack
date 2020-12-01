163f22964b51be971599643a05b404c1c17daec0
alicia-myob <Alicia.Shen@myob.com>
2020-12-01 14:14:37 +1300

    [062][fix] Removing unused worth variable

--- BlackJack/Hand.cs
+++ BlackJack/Hand.cs
@@ -6,11 +6,9 @@ namespace BlackJack_Kata
     public class Hand
     {
         private List<Card> _hand = new List<Card>();
-        private int _worth;
+        
         public Hand()
-        {
-            _worth = 0;
-        }
+        {}
 
         public void AddCard(Card card)
         {
