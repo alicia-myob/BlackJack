68cfab5f16a54da0e73cf35fe415766d0bf4c1f1
alicia-myob <Alicia.Shen@myob.com>
2020-12-01 14:16:10 +1300

    [064][test] ShufflingIsConsistentWithMocking test

--- Tests/UnitTests/TestShuffler.cs
+++ Tests/UnitTests/TestShuffler.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Linq;
 using BlackJack_Kata;
+using FluentAssertions;
 using Moq;
 using Xunit;
 
@@ -10,19 +13,39 @@ namespace Tests.UnitTests
         public void ShuffleDeckDifferentToOriginalDeck()
         {
             //Arrange, Act and Assert
-            BasicShuffler shuffler = new BasicShuffler(new BasicRandomGenerator());
+            var shuffler = new BasicShuffler(new BasicRandomGenerator());
             var deck = CreateDeck();
             var shuffledDeck = shuffler.Shuffle(deck);
             Assert.NotEqual(deck, shuffledDeck);
         }
 
         [Fact]
-        public void ShufflerShouldShuffleCards()
+        public void ShufflingIsConsistentWithMocking()
         {
-            
+            var mock = new Mock<IRandomGenerator>();
+            mock.Setup(x => x.Generate(It.IsAny<int>())).Returns(12);
+            var shuffler = new BasicShuffler(mock.Object);
+            var deck1 = CreateDeck();
+            var deck2 = CreateDeck();
+            var shuffledDeck1 = shuffler.Shuffle(deck1);
+            var shuffledDeck2 = shuffler.Shuffle(deck2);
+            AssertingEqualityOnDecks(shuffledDeck1, shuffledDeck2);
         }
 
-        
+        private void AssertingEqualityOnDecks(Card[] deck1, Card[] deck2)
+        {
+            Assert.NotNull(deck1);  
+            Assert.NotNull(deck2);
+            Assert.Equal(deck1.Length, deck2.Length);
+            for (var i = 0; i < deck1.Length; i++)
+            {
+                Assert.Equal(deck1[i].GetRank(), deck2[i].GetRank());
+                Assert.Equal(deck1[i].GetSuit(), deck2[i].GetSuit());
+            }
+            
+            
+                      
+        }
 
         private Card[] CreateDeck()
         {
